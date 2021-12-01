aec0f6106d48983bec6b3868591a53c05cbcac75
Rajat Rajput <rajputrajat@gmail.com>
2021-12-01 22:37:00 +0530

    make space for part 2 of the question

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,35 +1,32 @@
 use std::fs;
 
 fn main() {
-    let samples = read_input_file("day1/input.txt");
-    let increase_count = how_many_increases(samples.iter());
+    let samples = part1::read_input_file("day1/input.txt");
+    let increase_count = part1::how_many_increases(samples.iter());
     println!("increasing count: {}", increase_count);
 }
 
-pub fn how_many_increases<'a, I: Iterator<Item = &'a usize>>(samples: I) -> usize {
-    let mut count = 0;
-    let mut prev = usize::max_value();
-    for cur in samples {
-        if cur > &prev {
-            count += 1;
+mod part1 {
+    pub(crate) fn how_many_increases<'a, I: Iterator<Item = &'a usize>>(samples: I) -> usize {
+        let mut count = 0;
+        let mut prev = usize::max_value();
+        for cur in samples {
+            if cur > &prev {
+                count += 1;
+            }
+            prev = *cur;
         }
-        prev = *cur;
+        count
     }
-    count
-}
 
-fn read_input_file<'a>(input_file: &str) -> Vec<usize> {
-    let text = fs::read_to_string(input_file).unwrap();
-    let samples: Vec<usize> = text
-        .split_whitespace()
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect();
-    samples
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
+    pub(crate) fn read_input_file<'a>(input_file: &str) -> Vec<usize> {
+        let text = super::fs::read_to_string(input_file).unwrap();
+        let samples: Vec<usize> = text
+            .split_whitespace()
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect();
+        samples
+    }
 
     #[test]
     fn sample() {
@@ -37,3 +34,5 @@ mod tests {
         assert_eq!(how_many_increases(samples.iter()), 7);
     }
 }
+
+mod part2 {}
