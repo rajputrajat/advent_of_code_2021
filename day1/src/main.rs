5c0202607e00d04ead649ab1f5d9614b829f304a
Rajat Rajput <rajputrajat@gmail.com>
2021-12-01 21:45:27 +0530

    prepairing the test

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn how_many_increases<'a, I: Iterator<Item = &'a usize>>(samples: I) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        let samples = [199_usize, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(how_many_increases(samples.iter()), 7);
+    }
+}
