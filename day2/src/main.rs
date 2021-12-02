c3d877e015377caf495358e845c23acf6d0c9d59
Rajat Rajput <rajputrajat@gmail.com>
2021-12-02 20:48:08 +0530

    day2 test is passing

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+enum Movement {
+    Forward(i32),
+    Down(i32),
+    Up(i32),
+}
+
+impl Movement {
+    fn parse(line: &str) -> Self {
+        let mut split = line.trim().split_whitespace();
+        let act = split.next().unwrap();
+        let mov: i32 = split.next().unwrap().parse().unwrap();
+        match act {
+            "forward" => Movement::Forward(mov),
+            "down" => Movement::Down(mov),
+            "up" => Movement::Up(mov),
+            _ => unreachable!(),
+        }
+    }
+}
+
+fn get_product<'a, I: Iterator<Item = &'a str>>(input: I) -> i32 {
+    let mut forward = 0;
+    let mut depth = 0;
+    for line in input {
+        let mov = Movement::parse(line);
+        match mov {
+            Movement::Forward(f) => forward += f,
+            Movement::Down(d) => depth += d,
+            Movement::Up(u) => depth -= u,
+        };
+    }
+    forward * depth
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pos_mul() {
+        assert_eq!(150, get_product(TEST_INPUT.split('\n')));
+    }
+
+    const TEST_INPUT: &str = r##"forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"##;
 }
