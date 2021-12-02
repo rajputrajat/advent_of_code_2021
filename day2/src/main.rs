1b95b4de419982393a55bd6fae56afffb5c9bccb
Rajat Rajput <rajputrajat@gmail.com>
2021-12-02 21:30:08 +0530

    should get output of part2 too

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -7,8 +7,10 @@ fn main() -> Result<()> {
     let reader = BufReader::new(File::open("day2/input.txt")?);
     let input: Vec<_> = reader.lines().map(|s| s.unwrap()).collect();
     let moves = parse_input(&input);
-    let product = part1::get_product(&moves);
-    println!("product is: {}", product);
+    let product_part1 = part1::get_product(&moves);
+    println!("product is: {}", product_part1);
+    let improved_product = part2::get_product(&moves);
+    println!("improve product is '{}'", improved_product);
     Ok(())
 }
 
@@ -50,23 +52,46 @@ mod part1 {
         }
         forward * depth
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
 
     #[test]
     fn pos_mul() {
         let test_input: Vec<String> = TEST_INPUT.split('\n').map(|s| s.to_owned()).collect();
         let moves = parse_input(&test_input);
-        assert_eq!(150, part1::get_product(&moves));
+        assert_eq!(150, get_product(&moves));
+    }
+}
+
+mod part2 {
+    use super::*;
+    pub(super) fn get_product(moves: &[Movement]) -> i32 {
+        let mut forward = 0;
+        let mut depth = 0;
+        let mut aim = 0;
+        for m in moves {
+            match m {
+                Movement::Forward(f) => {
+                    forward += f;
+                    depth += aim * f;
+                }
+                Movement::Down(d) => aim += d,
+                Movement::Up(u) => aim -= u,
+            };
+        }
+        forward * depth
     }
 
-    const TEST_INPUT: &str = r##"forward 5
+    #[test]
+    fn pos_mul() {
+        let test_input: Vec<String> = TEST_INPUT.split('\n').map(|s| s.to_owned()).collect();
+        let moves = parse_input(&test_input);
+        assert_eq!(900, get_product(&moves));
+    }
+}
+
+#[cfg(test)]
+const TEST_INPUT: &str = r##"forward 5
 down 5
 forward 8
 up 3
 down 8
 forward 2"##;
-}
