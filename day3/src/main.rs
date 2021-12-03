a21ad8a5166126d021933a0b1f2156bc6b203306
Rajat Rajput <rajputrajat@gmail.com>
2021-12-03 20:03:17 +0530

    not happy with design. still testing. failed

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -81,9 +81,125 @@ fn get_power_consumption(input: &[usize]) -> usize {
     gamma_rate * epsilon_rate
 }
 
+fn parse_as_num_vec(input_str: &[String]) -> Vec<Vec<u8>> {
+    let vv_nums: Vec<Vec<u8>> = input_str
+        .iter()
+        .map(|s| s.chars().map(|c| c.to_string().parse().unwrap()).collect())
+        .collect();
+    vv_nums
+}
+
+mod part2 {
+    pub(super) fn get_life_support_rating(vec_nums: Vec<Vec<u8>>) -> usize {
+        let o2_rating = {
+            let mut reduced_indices: Vec<bool> =
+                std::iter::repeat(true).take(vec_nums.len()).collect();
+            loop {
+                let mut more_ones = (0, reduced_indices.clone());
+                let mut more_zeroes = more_ones.clone();
+                let mut bit_pos = 0;
+                reduced_indices
+                    .iter_mut()
+                    .filter(|x| **x)
+                    .enumerate()
+                    .for_each(|(i, _)| {
+                        if vec_nums[i][bit_pos] == 1 {
+                            more_ones.0 += 1;
+                            more_zeroes.1[i] = false;
+                        } else {
+                            more_zeroes.0 += 1;
+                            more_ones.1[i] = false;
+                        }
+                    });
+                reduced_indices = if more_ones.0 > more_zeroes.0 {
+                    more_ones.1
+                } else {
+                    more_zeroes.1
+                };
+                bit_pos += 1;
+                if reduced_indices.iter().filter(|&&x| x).count() == 1 {
+                    break;
+                }
+            }
+            let mut o2_index = 0;
+            reduced_indices.iter().enumerate().for_each(|(i, &x)| {
+                if x {
+                    o2_index = i;
+                }
+            });
+            binary_vec_to_num(&vec_nums[o2_index])
+        };
+
+        let co2_rating = {
+            let mut reduced_indices: Vec<bool> =
+                std::iter::repeat(true).take(vec_nums.len()).collect();
+            loop {
+                let mut more_ones = (0, reduced_indices.clone());
+                let mut more_zeroes = more_ones.clone();
+                let mut bit_pos = 0;
+                reduced_indices
+                    .iter_mut()
+                    .filter(|x| **x)
+                    .enumerate()
+                    .for_each(|(i, _)| {
+                        if vec_nums[i][bit_pos] == 1 {
+                            more_ones.0 += 1;
+                            more_zeroes.1[i] = false;
+                        } else {
+                            more_zeroes.0 += 1;
+                            more_ones.1[i] = false;
+                        }
+                    });
+                reduced_indices = if more_ones.0 < more_zeroes.0 {
+                    more_ones.1
+                } else {
+                    more_zeroes.1
+                };
+                bit_pos += 1;
+                if reduced_indices.iter().filter(|&&x| x).count() == 1 {
+                    break;
+                }
+            }
+            let mut co2_index = 0;
+            reduced_indices.iter().enumerate().for_each(|(i, &x)| {
+                if x {
+                    co2_index = i;
+                }
+            });
+            binary_vec_to_num(&vec_nums[co2_index])
+        };
+        o2_rating * co2_rating
+    }
+
+    fn binary_vec_to_num(bin_vec: &[u8]) -> usize {
+        bin_vec
+            .iter()
+            .rev()
+            .enumerate()
+            .fold(0_usize, |acc, (i, &x)| acc | (x as usize) << i)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        #[test]
+        fn vec_to_num() {
+            assert_eq!(binary_vec_to_num(&[1, 0, 0, 1, 0]), 18);
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn sample_str_to_num_vecs() {
+        assert_eq!(
+            parse_as_num_vec(&["11000".to_owned(), "10010".to_owned()]),
+            vec![vec![1, 1, 0, 0, 0], vec![1, 0, 0, 1, 0]]
+        );
+    }
 
     #[test]
     fn parse_bstr() {
@@ -96,6 +212,13 @@ mod tests {
         let mut out = vec![];
         count_bits(0b10110, &mut out);
         assert_eq!(out, vec![0_usize, 1, 1, 0, 1])
+    }
+
+    #[test]
+    fn life_support_rating() {
+        let num_input: Vec<String> = INPUT.split_whitespace().map(|s| s.to_owned()).collect();
+        let nvec = parse_as_num_vec(&num_input);
+        assert_eq!(230, part2::get_life_support_rating(nvec));
     }
 
     #[test]
