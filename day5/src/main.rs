d2705e6fee028098b38e96c089447263c6d0714b
Rajat Rajput <rajputrajat@gmail.com>
2021-12-05 16:55:30 +0530

    wip

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,5 +1,49 @@
 fn main() {}
 
+#[derive(Debug, Default, Clone, Copy)]
+struct Point {
+    x: u16,
+    y: u16,
+}
+
+#[derive(Debug, PartialEq)]
+enum LineType {
+    Horizontal,
+    Vertical,
+    Angled,
+}
+
+#[derive(Debug)]
+struct Line {
+    a: Point,
+    b: Point,
+    typ: LineType,
+}
+
+impl Line {
+    fn from_points(a: &Point, b: &Point) -> Self {
+        Self {
+            a: *a,
+            b: *b,
+            typ: if a.x == b.x {
+                LineType::Horizontal
+            } else if a.y == b.y {
+                LineType::Vertical
+            } else {
+                LineType::Angled
+            },
+        }
+    }
+
+    fn crosses(&self, other: &Line) -> Vec<Point> {
+        if self.typ == LineType::Horizontal && other.typ == LineType::Horizontal {
+            vec![]
+        } else {
+            vec![]
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
