9d8f8ef307d5f40dcc7ca989ac0839cea456d9fb
Rajat Rajput <rajputrajat@gmail.com>
2021-12-08 00:38:40 +0530

    prep for part2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let input = read_to_string("day7/input.txt").unwrap();
     let mut crabs = Crabs::from_text(&input);
     crabs.0.sort();
-    println!("part1, answer: '{}'", crabs.get_min());
+    println!("part1, answer: '{}'", crabs.get_min(|a| *a));
 }
 
 #[derive(Debug)]
@@ -38,22 +38,23 @@ impl Crabs {
         Self(crabs)
     }
 
-    fn get_diff_sum(&self, diff: i32) -> u32 {
+    fn get_diff_sum<F: Fn(&i32) -> i32>(&self, diff: i32, differ: F) -> u32 {
+        //let differ = |d: i32| d * (d + 1) / 2;
         self.0
             .iter()
-            .map(|&(num, count)| (num as i32 - diff).abs() as u32 * count)
+            .map(|&(num, count)| (num as i32 - differ(&diff)).abs() as u32 * count)
             .sum()
     }
 
-    fn get_min(&self) -> u32 {
+    fn get_min<F: Fn(&i32) -> i32 + Copy>(&self, differ: F) -> u32 {
         let avg = (self.0.iter().map(|(num, count)| num * count).sum::<u32>() / self.0.len() as u32)
             as i32;
-        let one_less = self.get_diff_sum(avg - 1);
-        let at_avg = self.get_diff_sum(avg);
+        let one_less = self.get_diff_sum(avg - 1, differ);
+        let at_avg = self.get_diff_sum(avg, differ);
         let mut min = u32::MAX;
         if one_less < at_avg {
             (0..one_less as u32).into_iter().rev().for_each(|num| {
-                let sum = self.get_diff_sum(num as i32);
+                let sum = self.get_diff_sum(num as i32, differ);
                 if sum < min {
                     min = sum;
                 } else {
@@ -62,7 +63,7 @@ impl Crabs {
             });
         } else {
             (at_avg as u32..).into_iter().for_each(|num| {
-                let sum = self.get_diff_sum(num as i32);
+                let sum = self.get_diff_sum(num as i32, differ);
                 if sum < min {
                     min = sum;
                 } else {
@@ -81,7 +82,7 @@ mod tests {
     #[test]
     fn day7_part1() {
         let crabs = Crabs::from_text(INPUT);
-        assert_eq!(37, crabs.get_min());
+        assert_eq!(37, crabs.get_min(|a| *a));
     }
 
     const INPUT: &str = "16,1,2,0,4,2,7,1,2,14";
