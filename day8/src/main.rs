8ada8598a356207eb63575f862aca42c2c37531b
Rajat Rajput <rajputrajat@gmail.com>
2021-12-09 09:38:18 +0530

    part1 is solved

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,9 +1,13 @@
-use std::str::FromStr;
+use std::{fs::read_to_string, str::FromStr};
 
-fn main() {}
+fn main() {
+    let text_input = read_to_string("day8/input.txt").unwrap();
+    let input = parse_input(&text_input);
+    println!("part1, answer: '{}'", get_identified_digit_count(&input));
+}
 
 fn parse_input(input_text: &str) -> Vec<(Vec<String>, Vec<String>)> {
-    let lines: Vec<Vec<String>> = input_text
+    input_text
         .trim()
         .lines()
         .map(|l| {
@@ -11,21 +15,10 @@ fn parse_input(input_text: &str) -> Vec<(Vec<String>, Vec<String>)> {
                 .split_whitespace()
                 .filter_map(|s| if s == "|" { None } else { Some(s.to_owned()) })
                 .collect();
-            assert!(vstr.len() == 10 || vstr.len() == 4);
-            vstr
+            assert!(vstr.len() == 14);
+            (vstr[0..10].to_owned(), vstr[10..14].to_owned())
         })
-        .collect();
-    let ret: Vec<(Vec<String>, Vec<String>)> = lines
-        .chunks_exact(2)
-        .map(|w| {
-            let a = &w[0];
-            assert_eq!(10, a.len());
-            let b = &w[1];
-            assert_eq!(4, b.len());
-            (a.clone(), b.clone())
-        })
-        .collect();
-    ret
+        .collect()
 }
 
 enum Digit {
@@ -81,24 +74,14 @@ mod tests {
         assert_eq!(26, get_identified_digit_count(&parsed_input));
     }
 
-    const INPUT: &str = r##"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb |
-fdgacbe cefdb cefbgd gcbe
-edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec |
-fcgedb cgb dgebacf gc
-fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef |
-cg cg fdcagb cbg
-fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega |
-efabcd cedba gadfec cb
-aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga |
-gecf egdcabf bgf bfgea
-fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf |
-gebdcfa ecba ca fadegcb
-dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf |
-cefg dcbef fcge gbcadfe
-bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd |
-ed bcgafe cdgba cbgef
-egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg |
-gbdfcae bgc cg cgb
-gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |
-fgae cfgab fg bagce"##;
+    const INPUT: &str = r##"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"##;
 }
