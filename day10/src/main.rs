36041cffd5c103f73fea88a622aed08db64639e8
Rajat Rajput <rajputrajat@gmail.com>
2021-12-10 21:28:16 +0530

    part2 test passed

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,6 +1,12 @@
-use std::collections::{HashMap, VecDeque};
+use std::{
+    collections::{HashMap, VecDeque},
+    fs::read_to_string,
+};
 
-fn main() {}
+fn main() {
+    let input_text = read_to_string("day10/input.txt").unwrap();
+    println!("part1 answer: {}", find_syntax_error_score(&input_text));
+}
 
 #[derive(Debug, PartialEq)]
 enum SymbolType {
@@ -40,6 +46,37 @@ fn find_syntax_error_score(input_text: &str) -> usize {
     errors.iter().fold(0, |acc, c| acc + map.get(c).unwrap())
 }
 
+fn find_middle_scope_incomplete(input_text: &str) -> usize {
+    let mut errors: Vec<usize> = Vec::new();
+    let pairs = HashMap::from([('[', ']'), ('{', '}'), ('(', ')'), ('<', '>')]);
+    let map: HashMap<char, usize> = HashMap::from([(')', 1), (']', 2), ('}', 3), ('>', 4)]);
+    for line in input_text.lines() {
+        let mut stack: VecDeque<SymbolType> = VecDeque::new();
+        for c in line.chars() {
+            match c {
+                '[' | '(' | '{' | '<' => stack.push_back(SymbolType::Opening(c)),
+                ']' | ')' | '}' | '>' => {
+                    let _ = stack.pop_back();
+                }
+                _ => unimplemented!(),
+            };
+        }
+        let score = stack
+            .iter_mut()
+            .rev()
+            .map_while(|sym| match sym {
+                SymbolType::Closing(_) => None,
+                SymbolType::Opening(sym) => Some(sym),
+            })
+            .fold(0, |acc, sym| {
+                acc * 5 + map.get(pairs.get(sym).unwrap()).unwrap()
+            });
+        errors.push(score);
+    }
+    errors.sort();
+    errors.iter().nth(errors.len() / 2).copied().unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -47,6 +84,11 @@ mod tests {
     #[test]
     fn day10_part1() {
         assert_eq!(26397, find_syntax_error_score(INPUT));
+    }
+
+    #[test]
+    fn day10_part2() {
+        assert_eq!(288957, find_middle_scope_incomplete(INPUT));
     }
 
     const INPUT: &str = r##"[({(<(())[]>[[{[]{<()<>>
